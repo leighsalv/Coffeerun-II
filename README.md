adb4fa807f6fcba1c4318b4892fff80eae6ed974
Leigh Salvador <lssalvador@csu.fullerton.edu>
2018-03-12 12:43:00 -0700

    Add/Delete from backend

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Coffeerun
 
-A simple e-commerce site that takes coffee orders for a food truck written in **HTML, CSS, Bootstrap, Javascript, and JQuery**. There is also a payment form that takes in users' contact information and credit card information. This is for **Homework 3 - CPSC 473 Front-End Development course**.
+A simple e-commerce site that takes coffee orders for a food truck written in **HTML, CSS, Bootstrap, Javascript, and JQuery**. **Deployd & MongoDB** is used for the backend to store/remove coffee orders. This is for **Homework 4 - CPSC 473 Front-End Development course**.
 
 **Functionalities & Features**
 
@@ -8,6 +8,7 @@ A simple e-commerce site that takes coffee orders for a food truck written in **
 - takes in user's email address
 - pending orders are shown as checkbox lists & removes it when checked
 - payment form where users can put their contact information & credit card information
+- customer's information & coffee order(s) are stored in the backend
 
 ## Getting Started
 
@@ -19,13 +20,18 @@ A simple e-commerce site that takes coffee orders for a food truck written in **
 
 `browser-sync` in terminal to automatically reload browser: `npm install -g browser-sync`
 
+[`MongoDB`](https://docs.mongodb.com/manual/administration/install-community/) for backend
+
+[`Deployd`](https://github.com/deployd/deployd#install-from-npm) for backend
+
 ### Run In Terminal
 
-`cd/directory-of-coffeerun/Coffeerun-master` change _directory-of-coffeerun_ to the directory in which you downloaded the repo
+`cd/directory-of-coffeerun/Coffeerun2-master` change _directory-of-coffeerun_ to the directory in which you downloaded the repo
 
 `browser-sync start --server --browser "Google Chrome" --files "*.html, stylesheets/*.css, scripts/*.js"` to view Coffeerun on browser.
 
+open a new terminal (CMD+T or CNTRL+T) and run deployd `dpd -d` in `cd/Coffeerun2-master/coffeerun-backend`
 
-<!-- ## Demo -->
+## Demo
 
-<!-- ![alt text](img/ottergramDemo.gif) -->
+![alt text](Coffeerun2-Demo.gif)
--- scripts/remotedatastore.js
+++ scripts/remotedatastore.js
@@ -6,7 +6,7 @@
 
   // Constructor checks valid url
   function RemoteDataStore(url) {
-    if(!url) {
+    if (!url) {
       throw new Error("No remote URL supplied.");
     }
     this.serverUrl = url;
@@ -17,8 +17,20 @@
   //the URL of the server to send the request to and what data to include.
   //key argument is not used, but kept so it is identical to the add menthod of DataStore
   RemoteDataStore.prototype.add = function(key, val) {
-    $.post(this.serverUrl, val, function(serverResponse){
-      console.log(serverResponse);
+    // $.post(this.serverUrl, val, function(serverResponse) {
+    //   console.log(serverResponse);
+    // });
+
+    $.ajax(this.serverUrl, {
+      type: "POST",
+      contentType: "application/json",
+      data: JSON.stringify(val),
+      success: function(serverResponse) {
+        console.log(serverResponse);
+      },
+      error: function(xhr) {
+        alert(xhr.responseText);
+      }
     });
   };
 
@@ -26,23 +38,62 @@
   //having a 'cb' (callback) as a param lets us access func argument & server response
   //$.get: dont pass data b/c we are retrieving information instead of saving info
   RemoteDataStore.prototype.getAll = function(cb) {
-    $.get(this.serverUrl, function(serverResponse){
-      console.log(serverResponse);
-      cb(serverResponse);
+    // $.get(this.serverUrl, function(serverResponse) {
+    //   console.log(serverResponse);
+    //   cb(serverResponse);
+    // });
+    $.ajax(this.serverUrl + "coffeeorders/" , {
+      type: "GET",
+      success: function(serverResponse) {
+        console.log(serverResponse);
+        cb(serverResponse);
+      },
+      error: function(xhr) {
+        console.log(xhr.responseText);
+      }
     });
   };
 
   //.get: gets a single coffee order
   RemoteDataStore.prototype.get = function(key, cb) {
-    $.get(this.serverUrl + "/" + key, function(serverResponse){
-      console.log(serverResponse);
-      cb(serverResponse);
+    // $.get(this.serverUrl + "/" + key, function(serverResponse) {
+    //   console.log(serverResponse);
+    //   cb(serverResponse);
+    //});
+    $.ajax("coffeeorders" + "/" + key, {
+      type: "GET",
+      success: function(serverResponse) {
+        console.log(serverResponse);
+        cb(serverResponse);
+      },
+      error: function(xhr) {
+        console.log(xhr.responseText);
+      }
     });
   };
 
   RemoteDataStore.prototype.remove = function(key) {
-    $.ajax(this.serverUrl + "/" + key, {
-      type: "DELETE"
+    // $.ajax(this.serverUrl + "/" + key, {
+    //   //type: "DELETE"
+    //   type: "POST",
+    //   url: this.serverUrl + "/" + key,
+    //   data: {_method:"DELETE"}
+    // });
+
+    $.ajax(this.serverUrl + "?emailAddress=" + key, {
+      type: "GET",
+      dataType: "json",
+      success: function(serverResponse) {
+        $.ajax("http://localhost:2403/coffeeorders" + "/" + serverResponse[0].id, {
+          type: "DELETE",
+          error: function(xhr) {
+            alert(xhr.responseText);
+          }
+        });
+      },
+      error: function(xhr) {
+        alert(xhr.responseText);
+      }
     });
   };
 
